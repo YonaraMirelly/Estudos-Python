b80dd6f0a74224324eefbe0caba09178927fd031
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-01-23 15:58:32 -0300

    commit

--- sorteio_ordem.py
+++ sorteio_ordem.py
@@ -5,5 +5,5 @@ n3 = input('Terceiro nome: ')
 n4 = input('quarto nome: ')
 lista = [n1, n2, n3, n4]
 shuffle(lista)
-print('A ordem de apresentação será:\n \033[1;4;32;41m{}\033[m.'.format(lista))
+print('A ordem de apresentação será:\n\033[1;4;32;41m{}\033[m.'.format(lista))
 input()
