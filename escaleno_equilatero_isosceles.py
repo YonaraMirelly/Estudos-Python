a08938be988363e2bd547a390bb45cf05c2251b2
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-01-23 17:29:56 -0300

    commit

--- escaleno_equilatero_isosceles.py
+++ escaleno_equilatero_isosceles.py
@@ -1,7 +1,7 @@
 #desafio dos três triângulos
-print('\033[1;31m+\033[m'*20)
+print('\033[1;31m+\033[m'*24)
 print('Analisador de Triângulos')
-print('\033[33m=\033[m'*20)
+print('\033[33m=\033[m'*24)
 a = float(input('\033[1mPrimeira reta do triângulo:\033[m '))
 b = float(input('\033[1;31mSegunda reta do triângulo:\033[m '))
 c = float(input('\033[1;34mTerceira reta do triângulo:\033[m '))
