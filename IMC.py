bfa64eda1a5039acc2c0eae024cad38515e4c0d2
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-01-23 17:33:01 -0300

    commit

--- IMC.py
+++ IMC.py
@@ -6,13 +6,13 @@ imc = p / (a ** 2)
 print('Seu IMC é de {:.1f}.'.format(imc))
 #condições aninhadas
 if imc <= 18.5:
-    print('Você está ABAIXO DO PESO.')
+    print('Você está \033[1;4mABAIXO DO PESO.\033[m')
 elif imc > 18.5 and imc <= 25:
-    print('Você está no PESO IDEAL.')
+    print('Você está no \033[1;4mPESO IDEAL.\033[m')
 elif imc > 25 and imc <= 30:
-    print('Você está no SOBREPESO.')
+    print('Você está no \033[1;4mSOBREPESO.\033[m')
 elif imc > 30 and imc <=40:
-    print('Você está na OBESIDADE.')
+    print('Você está na \033[1;4mOBESIDADE.\033[m')
 else:
     print('\033[1;31mATENÇÃO: OBESIDADE MÓRBIDA!\033[m')
 input()
