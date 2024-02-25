16b99422bf13e369f7b139a68981fb0457e05772
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-02-25 09:24:14 -0300

    commit

---
+++ Repetição_While/ex1.py
@@ -0,0 +1,35 @@
+#n = int(input("Digite um valor-> "))
+#a, b = 0, 1
+#while a<=n:
+#    print(a, end = ' - ')
+#    a, b = b, a + b
+#print('fim')
+
+#x = 10
+#while x >=0:
+#    x-=1
+#    if x%2 != 0:
+#        continue
+#    print(f'{x} é par')
+#else:
+#    print('fim')
+
+#def Fibonacci(n):
+#    a, b = 0, 1
+#    while a <=n:
+#        print(a, end = ' - ')
+#        a, b = b, a+b
+#    print('fim')
+#Fibonacci(13)
+
+#def Par(x):
+#    while x >=0:
+#        x-=1
+#        if x%2 !=0:
+#            continue
+#        print(f'{x} é par')
+#    else:
+#        print('fim')
+#Par(20)
+
+
