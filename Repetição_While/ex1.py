c833bdc0dbfe8bd4a027ba57dbce54f9556289a0
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-02-25 10:31:23 -0300

    commit

--- Repetição_While/ex1.py
+++ Repetição_While/ex1.py
@@ -32,4 +32,61 @@
 #        print('fim')
 #Par(20)
 
+#cont = 0
+#sair = 1
+#while sair != 0:
+#    idade = int(input('idade: '))
+#    altura = float(input('altura: '))
+#    sair = int(input('Sair? [0] '))
+#    print('-'*30)
+#    if (idade>13) and (altura<1.5):
+#        cont +=1
+#print(f'{cont} pessoas tem idade maior que 13 e possuem altura inferior a 1.5m')
+
+#def Maior13():
+#    cont = 0
+#    sair = 1
+#    while sair !=0:
+#        idade = int(input('idade: '))
+#        altura = float(input('altura: '))
+#        sair = int(input('sair? '))
+#        print('-'*30)
+#        if (idade>13) and (altura<1.5):
+#            cont +=1
+#    print(f'{cont} pessoas tem idade maior que 13 e possuem altura inferior a 1.5m')
+#Maior13()
+
+#l = []
+#while True:
+#    n = int(input('número-> '))
+#    sair = int(input('sair [99] '))
+#    if sair == 99:
+#        break
+#    elif n %2 == 0 or n<0:
+#        continue
+#    l.append(n)
+#print(f'{l} é ímpar positivo.')
+
+#def Impar():
+#    l = []
+#    while True:
+#        n = int(input('núemro-> '))
+#        sair = int(input('sair? [99] '))
+#        if sair == 99:
+#            break
+#        elif n%2 == 0 or n<0:
+#            continue
+ #       l.append(n)
+#    print(f'{l} são numeros impares positivos.')
+#Impar()
+
+n = int(input('Fatorial de -> '))
+c = n
+f = 1
+while c>0:
+    print(f'{c}', end = '')
+    print(' x ' if c >1 else ' = ', end = '')
+    f *=c
+    c-=1
+print(f'{f}')
 
