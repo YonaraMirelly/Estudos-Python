ea3ab088cd8ba49603d1377b50bb78680018886a
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-02-26 19:10:03 -0300

    commit

---
+++ Funções/ex1.py
@@ -0,0 +1,24 @@
+#def multi(a,b,c):
+#    return a*b*c
+#print(multi(1,2,3))
+
+#def Fatorial(num):
+#    c = num
+#    f = 1
+#    while c>0:
+#        print(f'{c}', end = '')
+#        print(' x ' if c>1 else ' = ', end = '')
+#        f*=c
+#        c-=1
+#    print(f'{f}')
+#Fatorial(5)
+
+#def fibonacci(n):
+#    a = 0
+#    b = 1
+#    while a<=n:
+#        print(a, end = ' - ')
+#        a,b = b, a+b
+#fibonacci(5)
+
+
