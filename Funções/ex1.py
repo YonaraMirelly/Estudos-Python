f3744920e28ca51c7b682ad4a10eeca5e9ab4faf
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-02-26 19:47:54 -0300

    commit

--- Funções/ex1.py
+++ Funções/ex1.py
@@ -22,3 +22,27 @@
 #fibonacci(5)
 
 
+#def Concatena(*args):
+#    tamanho = len(args)
+#    conca = ''.join(map(str,args))
+#    return tamanho, conca
+#print(Concatena(1,2,'yonara', 'mirelly'))
+    
+
+#def Remover(lista):
+#    palavras_filtradas = [palavra for palavra in lista if not palavra.lower().startswith('a')]
+#    return palavras_filtradas   
+#lista = ['maçâ', 'amanhâ', 'Amanda']
+#print(Remover(lista))
+
+#def Quadrado(intervalo):
+#    quadrado = [x**2 for x in range(intervalo) if x %2==0]    
+#    return quadrado
+#print(Quadrado(11))
+
+def Digito(numero):
+    s = str(numero)
+    a = len(s)
+    return a
+
+print(Digito(12349))
