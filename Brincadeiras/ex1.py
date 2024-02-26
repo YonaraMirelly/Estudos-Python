4729e645eaf1129961ce559fd6bc6a91ae7bec22
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-02-26 10:58:47 -0300

    commit

---
+++ Brincadeiras/ex1.py
@@ -0,0 +1,47 @@
+#s1 = str(input('string1- '))
+#s2 = str(input('string2 - '))
+#print(f'comprimento {s1} = {len(s1)}')
+#print(f'comprimento {s2} = {len(s2)}')
+#if len(s1) == len(s2):
+#    print('têm o mesmo comprimento')
+#else:
+#    print('não têm o mesmo comprimento')
+#if s1 == s2:
+#    print('literalmente idênticas.')
+
+#def String(s1,s2):
+#    print(f'comprimento de {s1} = {len(s1)}')
+#    print(f'comprimento de {s2} = {len(s2)}')
+#    if len(s1) == len(s2):
+#        print('têm o mesmo comprimento')
+#    else:
+#        print('não tem o mesmo comprimento')
+#    if s1 == s2:
+#        print('idênticos!')
+#String('yona', 'yonar')
+
+#nome = str(input('nome: '))
+#novo = nome[::-1].upper()
+#print(novo)
+
+#def Nome(nome):
+#    novo = nome[::-1].upper()
+#    print(novo)
+#Nome('yonara')
+
+#nome = str(input('nome - '))
+#for letra in nome:
+#    print(letra)
+
+#nome = str(input('nome - '))
+#for i in range(len(nome)+1):
+#    print(nome[:i])
+
+#def Maneiro(nome):
+#    for i in range(len(nome)+1):
+#        print(nome[:i])
+#Maneiro('poly')
+
+#s = str(input('frase: '))
+#print(s.count('ado'))
+
