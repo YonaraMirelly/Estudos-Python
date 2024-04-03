2fae42d73ffa6c3f12bc7de51a6627fc78acc8cd
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-04-02 22:24:07 -0300

    commit

--- Estudos_Rodrigo/copa.py
+++ Estudos_Rodrigo/copa.py
@@ -1,14 +1,13 @@
 total = int(input())
 n = int(input())
 if 1 <= total <= 100 and 1 <= n <=300:
-    lista = [int(input('dale -> ')) in range(n)]
+    lista = [i for i in range(1, total +1)]
     contador = 0
-    for x in range(n):
-        if numero in lista:
-            lista.remove(numero)
-            contador += 0
-
-
+    for _ in range(n):
+        figurinha = int(input('dale -> '))
+        if figurinha in lista:
+            lista.remove(figurinha)
+            contador += 1
     falta = total - contador
     print(falta)
 else:
---
+++ Estudos_Rodrigo/domino.py
@@ -0,0 +1,6 @@
+n = int(input())
+if  0 <= n <= 1000:
+    calculo = (n+1)*(n+2)/2
+    print(int(calculo))
+else:
+    print('digite corretamente!')
