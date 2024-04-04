d38307febf58e17bdc6676ed45ad2af494fe647d
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-04-04 11:55:38 -0300

    commit

--- Repetição_For/for_fatorial.py
+++ Repetição_For/for_fatorial.py
@@ -1,9 +1,25 @@
-n = int(input('Digite um número para calcular o seu fatorial: '))
-f = 1
-print('Calculando {}! = '.format(n), end='')
-for i in range(n, 0 ,-1):
-    print(i,end='')
-    f = f * i
-    print(' x ' if i >1 else ' = ', end='')
-print('{}'.format(f))
-input()
+#n = int(input('Digite um número para calcular o seu fatorial: '))
+#f = 1
+#print('Calculando {}! = '.format(n), end='')
+#for i in range(n, 0 ,-1):
+#    print(i,end='')
+#    f = f * i
+#    print(' x ' if i >1 else ' = ', end='')
+#print('{}'.format(f))
+#input()
+
+#def fat(n):
+ #   if n<2:
+ #       return 1
+ #   else:
+ #       return n * fat(n-1)
+    
+#fat(5)
+
+def fib(n):
+    if n <=2:
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
+
+print(fib(30))
