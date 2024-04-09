76d8af5f21416acf6b99b3727bfe0906a1318cd5
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-04-09 19:04:48 -0300

    commit

---
+++ Estudos_Rodrigo/peça_perdida.py
@@ -0,0 +1,8 @@
+n = int(input())
+if 2 <= n <= 1000:
+    lista = [int(i) for i in input().split()]
+    falta = [x for x in range(1, n+1) if x not in lista]
+    for x in falta:
+        print(x)
+else:
+    print("Digite um número entre 2 e 1000.")
--- Repetição_For/for_fatorial.py
+++ Repetição_For/for_fatorial.py
@@ -16,10 +16,3 @@
     
 #fat(5)
 
-def fib(n):
-    if n <=2:
-        return 1
-    else:
-        return fib(n-1)+fib(n-2)
-
-print(fib(6))
