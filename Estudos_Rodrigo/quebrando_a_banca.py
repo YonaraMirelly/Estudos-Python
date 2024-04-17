9aa7c4050f25dc9f88c952d3fb78c512af80907c
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-04-17 17:26:23 -0300

    commit

--- Estudos_Rodrigo/quebrando_a_banca.py
+++ Estudos_Rodrigo/quebrando_a_banca.py
@@ -1,47 +1,30 @@
-
-
-
-# a, b = [int(i) for i in input().split()]
-# n = int(input('Número a ser analisado -> '))
-# digitos = [int(d) for d in str(n)]
-
-# maior = 0
-# lista_maiores =[]
-
-# while len(lista_maiores) != (a-b):
-#     for c in digitos:
-#         if c>maior:
-#             maior=c
-#     digitos = [x for x in digitos if x != maior]
-#     lista_maiores += [maior]
-#     maior = 0
-
-# for j in lista_maiores:
-#     print(j, end='')
-
-def maior(a, b, lista):
-    qtde_dig = a-b
-    maior = 0
-    maiores = []
-    while len(maiores) != qtde_dig:
-        for c in lista:
-            if c>maior:
-                maior=c
-        lista = [x for x in lista if x != maior]
-        maiores += [maior]
-        maior = 0
-    return maiores
-
-casos = []
-
-a, b = [int(i) for i in input().split()]
-n = int(input('Número a ser analisado -> '))
-digitos = [int(d) for d in str(n)] 
-casos += [maior(a, b, digitos)]
-
-for caso in casos:
-    for digito in caso:
-        print(digito, end='')
-    print()
+def Maior():
+    casos = []
+    while True:
+        entrada = input('A e B -> ')
+        if entrada == '':
+            break
+        else:
+            L = entrada.split()
+            listaAB = [int(i) for i in L]
+            digitos = [int(i) for i in input('Número a ser analisado -> ')]
+            limite = listaAB[0] - listaAB[-1]
+            c = 0
+            lista_Geral = [None] * limite
+            for i in range(limite):
+                c = 0
+                for j in digitos:
+                    if j > c:
+                        c = j
+                        lista_Geral[i] = c
+                digitos.remove(c)
+            numero = ''
+            for i in lista_Geral:
+                numero+=str(i)
+            casos += [numero]
+            numero = ''
     
+    for x in casos:
+        print(x)
 
+Maior()
