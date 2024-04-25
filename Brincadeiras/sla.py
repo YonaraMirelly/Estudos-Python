e28e29a6c126c0d419096653f76068b18c6dab03
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-04-25 07:17:35 -0300

    commit

--- Brincadeiras/sla.py
+++ Brincadeiras/sla.py
@@ -1,38 +1,10 @@
-n = int(input())
-lista = []
-for i in range(n):
-    lista += [[int(i) for i in input().split()]]
-
-soma_linhas = 0
-for x in lista[0]:
-    soma_linhas += x
-soma_reserva = 0
-# analisa todas as linhas
-for i in range(1, n):
-    for x in lista[i]:
-        soma_reserva += x
-        if soma_reserva != soma_linhas:
-            print(-1)
-# analisa todas as colunas
-soma_coluna = 0
-for j in range(n):
-    for i in range(n):
-        for x in lista[i][j]:
-            soma_coluna += x
-            if soma_coluna != soma_linhas:
-                print(-1)
-
-# primeira diagonal
-soma_diagonal = 0
-for i in range(n):
-    for i in range(n):
-        for x in lista[i][i]:
-            soma_diagonal += x
-            if soma_diagonal != soma_linhas:
-                print(-1)
-
-# segunda diagonal
-soma_diagonal2 = 0
-for i in range(n):
-    for i in range(n):
-        for x in lista[i][n-i-1]
+def main():
+    lista = [int(i) for i in input().split()]
+    num = int(input())
+    if num in lista:
+        print('SIM')
+    else:
+        print('NÃO')
+    return ''
+if __name__ == "__main__":
+    print(main())
