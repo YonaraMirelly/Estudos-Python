b638be0b5f753d30ad6d922074d4f41c34a2904b
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-04-25 17:41:45 -0300

    commit

--- Estudos_Rodrigo/peça_perdida.py
+++ Estudos_Rodrigo/peça_perdida.py
@@ -1,8 +1,11 @@
+# n = int(input())
+# if 2 <= n <= 1000:
+#     lista = [int(i) for i in input().split()]
+#     falta = [x for x in range(1, n+1) if x not in lista]
+#     for x in falta:
+#         print(x)
+# else:
+#     print("Digite um número entre 2 e 1000.")
+
 n = int(input())
-if 2 <= n <= 1000:
-    lista = [int(i) for i in input().split()]
-    falta = [x for x in range(1, n+1) if x not in lista]
-    for x in falta:
-        print(x)
-else:
-    print("Digite um número entre 2 e 1000.")
+incompleta = [int(i) for i in input().split()]
