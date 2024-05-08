02088b4c98ddb5da6dfa1527a33e9b869db42975
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-05-08 18:31:59 -0300

    commit

--- Brincadeiras/sla.py
+++ Brincadeiras/sla.py
@@ -4,13 +4,14 @@ for _ in range(qtde):
     numero = int(input())
     lista += [numero]
 
-junto = int(''.join(str(num) for num in lista))
+junto = ''.join(str(num) for num in lista)
 dic = {}
-cont = 0
-for i in range(0, 9):
-    if i in junto:
-        cont +=1
-        dic[i] = cont
 
-for chave, valor in dic:
-    print('f{chave} - {valor}')
+for i in range(10):
+    dic[str(i)] = 0
+
+for digito in junto:
+    dic[digito] +=1
+
+for chave, valor in dic.items():
+    print(f'{chave} - {valor}')
