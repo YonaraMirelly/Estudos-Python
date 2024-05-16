831b3dcd7087847d683a2224f76a02b2ce1c55c5
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-05-16 12:09:26 -0300

    .

--- Brincadeiras/sla.py
+++ Brincadeiras/sla.py
@@ -1,9 +1,13 @@
-string = str(input())
-vogais = ['a', 'e', 'i', 'o', 'u']
-risada = ''.join(x for x in string if x in vogais)
+posi = [int(i) for i in input().split()]
+corre = posi[-1]
 
-
-if risada == risada[::-1]:
+if posi[0] < posi[1] and posi[1] > posi[2] > posi[0] and corre == -1:
+    print('S')
+elif posi[2] > posi[0] and posi[2] > posi[1] and corre == 1:
+    print('S')
+elif posi[2] < posi[0] and posi[2] < posi[1] and corre == 1:
+    print('S')
+elif posi[0] > posi[1] and posi[1] > posi[2] < posi[0] and corre == -1:
     print('S')
 else:
-    print('N')
+    print("N")
