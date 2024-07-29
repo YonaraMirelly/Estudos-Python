0c313a8909a3ba22033a06e6bed566c97429c998
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-07-29 06:32:31 -0300

    commit

--- Brincadeiras/aleatorio.py
+++ Brincadeiras/aleatorio.py
@@ -1,52 +1,24 @@
-# def topo(peso=0):
-#     print('-' * 20)
-#     print(f'o maior peso até agora é {peso}')
-#     print('-' * 20)
-# def rodada():
-#     peso = 0
-#     nome_pesado = ''
-#     topo(peso)
-#     for i in range(0,3):
-#         nome = input('nome da pessoa: ')
-#         p = float(input(f'Peso de {nome}: '))
-#         if p > peso:
-#             peso = p
-#             nome_pesado = nome
-#         topo(peso)
-#     topo(peso)
-#     print(f'A pessoa mais pesada foi {nome_pesado} com {peso}kg.')
-# rodada()
+from random import randint
+linha = int(input('linha= '))
+coluna = int(input('coluna = '))
+matriz = []
+soma = 0
+produto = 1
+for l in range(linha):
+    li = []
+    for c in range(coluna):
+        if l == c:
+            n = randint(0,9)
+            li += [n]
+            soma += n
+        n = randint(0, 9)
+        li += [n]
+    matriz += [li]
 
-# def soma(a,b):
-#     print(f'recebi {a}')
-#     print(f'recebi {b}')
-#     print(f'somando {a} + {b} = {a + b}')
-# soma(5,5)
+print('MATRIZ...')
+for l in range(linha):
+    for c in range(coluna):
+        print(f'[{matriz[l][c]:2}]', end = '')
+    print()
 
-# def par_impar(a):
-#     if a % 2 == 0:
-#         return 'par'
-#     else:
-#         return 'impar'
-# print(par_impar(4))
-
-# def soma(a, b):
-#     a +=1
-#     b +=2
-#     print(f'a soma vale {a+b}')
-#     return a, b
-# x = 4
-# y = 8
-# x, y = soma(x, y)
-# print(x,y)
-
-def fib(n):
-    a, b = 0, 1
-    lista = []
-    for i in range(1, n+1):
-        lista += [a]
-        a, b = b, a + b
-
-    return lista
-
-print(fib(13))
+print(f'SOMA DOS VALORES DA DIAGONAL PRINCIPAL = {soma} ')
